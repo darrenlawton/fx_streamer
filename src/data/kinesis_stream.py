6800ea924980da319e75f77f95a001119afc16b9
Darren Lawton <darren@darrenlawton.com>
2019-08-22 23:59:31 +1000

    minor changes.

--- src/data/kinesis_stream.py
+++ src/data/kinesis_stream.py
@@ -1,10 +1,9 @@
-# https://docs.aws.amazon.com/streams/latest/dev/tutorial-stock-data-kplkcl.html
+
 # https://github.com/bufferapp/kiner/blob/master/kiner/producer.py
-# https://github.com/awslabs/amazon-kinesis-client-python
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/kinesis.html
-# https://boto3.amazonaws.com/v1/documentation/api/latest/guide/examples.html
 # MAIN: https://github.com/aws-samples/amazon-kinesis-learning/blob/learning-module-1/src/com/amazonaws/services/kinesis/samples/stocktrades/writer/StockTradesWriter.java
-# READ: https://blog.sqreen.com/streaming-data-amazon-kinesis/
+# ACCOMPANIED: https://blog.sqreen.com/streaming-data-amazon-kinesis/
+# https://blog.sqreen.com/streaming-data-amazon-kinesis/
 
 import boto3
 import argparse, os, time
@@ -60,24 +59,3 @@ if __name__ == '__main__':
         kinesis_stream = kinesisStream(stream_name, n_shards, aws_profile)
     else:
         kinesis_stream = kinesisStream(stream_name, n_shards)
-
-
-# try:
-#     client = boto3.client('kinesis',region_name="us-east-2")
-#     response = client.create_stream(StreamName='TwitterStream',ShardCount=1)
-#
-#     print('stream {} created'.format(stream_name))
-# except ResourceInUseException:
-#     print('stream {} already exists'.format(stream_name))
-#     client.delete_stream(StreamName='TwitterStream')
-#
-# status = 'not set'
-# while( status != 'ACTIVE' )
-#     describe_stream_response = client.describe_stream(stream_name)
-#     description = describe_stream_response.get('StreamDescription')
-#     status = description.get('StreamStatus')
-#     time.sleep(1)
-#
-# api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
-#
-# kinesis = boto3.client('kinesis')
