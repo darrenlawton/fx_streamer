d6ef169ef264e2ea142e2041ef4d103209fcff6f
Darren Lawton <darren@darrenlawton.com>
2019-08-27 04:37:41 +1000

    some progress on av api for fx rates.

--- src/data/alpha_vantage/generator.py
+++ src/data/alpha_vantage/generator.py
@@ -1,6 +1,96 @@
 # https://codereview.stackexchange.com/questions/188753/pull-stock-data-from-alpha-vantage-api
 # https://github.com/RomelTorres/alpha_vantage/blob/develop/alpha_vantage/foreignexchange.py
-import os
+
+# main: https://github.com/Pmlsa/AlphaVantage/tree/master/AlphaVantage
+# understand this: https://stackoverflow.com/questions/14132789/relative-imports-for-the-billionth-time/14132912#14132912
+
+import json, urllib3
+from multiprocessing.pool import ThreadPool
+from src.data import data_config as dc
 
 
-os.getenv('ALPHA_VANTAGE_KEY')
+# # Public Methods
+# def fxrate(self, from_currency: str, to_currency: str = 'USD', **kwargs) -> DataFrame or None:
+#     """Simple wrapper to _av_api_call method for currency requests."""
+#
+
+# def _av_api_call(self, parameters: dict, timeout: int = 60, **kwargs) -> DataFrame or json or None:
+#     """Main method to handle AlphaVantage API call request and response."""
+#
+#     proxies = kwargs['proxies'] if 'proxies' in kwargs else self.proxy
+#
+#     # Everything is ok so far, add the AV API Key
+#     parameters['apikey'] = self.api_key
+#
+#     if not self.premium and self._api_call_count > 0:
+#         time.sleep(15.01)
+#
+#     # Ready to Go. Format and get request response
+#     try:
+#         response = requests.get(  # Use till self._requests_session can be mocked in unittests
+#             AlphaVantage.END_POINT,
+#             params=parameters,
+#             timeout=timeout,
+#             proxies=proxies
+#         )
+#     # except requests.RequestException as ex:
+#     except requests.exceptions.RequestException as ex:
+#         print(f"[X] response.get() exception: {ex}\n    parameters: {parameters}")
+#         pass
+#     finally:
+#         response.close()
+#
+#     if response.status_code != 200:
+#         print(f"[X] Request Failed: {response.status_code}.\nText:\n{response.text}\n{parameters['function']}")
+#
+#     # If 'json' datatype, return as 'json'. Otherwise return text response for 'csv'
+#     if self.datatype == 'json':
+#         response = response.json()
+#     else:
+#         response = response.text
+
+#     if self._api_call_count < 1:
+#         self._api_call_count += 1
+
+class fxClient:
+
+    def __init__(self):
+        self.session = urllib3.HTTPConnectionPool(dc.AV_HOST)
+        self.api_key = dc.validate_key(dc.AV_API_KEY)
+
+    def get_fx_rate(self, from_currency: str, to_currency: str = 'USD'):
+
+        #     parameters = {
+        #         'function': 'CURRENCY_EXCHANGE_RATE',
+        #         'from_currency': from_currency.upper(),
+        #         'to_currency': to_currency.upper()
+        #     }
+        #
+        #     download = self._av_api_call(parameters, **kwargs)
+        #     return download if download is not None else None
+
+        return None
+
+    def av_api_request(self):
+
+
+        return None
+
+    def get_fx_batch(self, from_currencies, to_currency):
+        """
+        threaded request for retrieving multiple
+        :param from_currencies: a list of currencies
+        :param to_currency: a string being the base currency
+        :returns: list fx rates in json format
+        """
+
+        return None
+
+    def threaded_requests(self):
+        # pool = ThreadPool(len(symbols))
+        # results = pool.map(function, symbols)
+        # pool.close()
+        # pool.join()
+        #
+        # return results
+        return None
--- src/data/data_config.py
+++ src/data/data_config.py
@@ -1,9 +1,15 @@
-import os
+import os, sys
 
 # Alpha Vantage
 AV_HOST = 'www.alphavantage.co'
 AV_API_KEY = os.getenv('ALPHA_VANTAGE_KEY')
-print(AV_API_KEY) # - NEED TO MAKE THIS PERMANENT
+
+
+def validate_key(api_key):
+    if api_key is None:
+        print("Alpha Vantage API key not set as ENV variable.")
+        sys.exit()
+    else: return api_key
 
 # AWS Kinesis Stream
 
