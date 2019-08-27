fcaa75f3395b406ec21561b2a6ee865a756fe522
Darren Lawton <darren@darrenlawton.com>
2019-08-27 13:41:29 +1000

    av_request done, need to test.

--- src/data/alpha_vantage/generator.py
+++ src/data/alpha_vantage/generator.py
@@ -9,45 +9,6 @@ import json, urllib3
 from multiprocessing.pool import ThreadPool
 from src.data import data_config as dc
 
-
-# def _av_api_call(self, parameters: dict, timeout: int = 60, **kwargs) -> DataFrame or json or None:
-#     """Main method to handle AlphaVantage API call request and response."""
-#
-#     proxies = kwargs['proxies'] if 'proxies' in kwargs else self.proxy
-#
-#     # Everything is ok so far, add the AV API Key
-#     parameters['apikey'] = self.api_key
-#
-#     if not self.premium and self._api_call_count > 0:
-#         time.sleep(15.01)
-#
-#     # Ready to Go. Format and get request response
-#     try:
-#         response = requests.get(  # Use till self._requests_session can be mocked in unittests
-#             AlphaVantage.END_POINT,
-#             params=parameters,
-#             timeout=timeout,
-#             proxies=proxies
-#         )
-#     # except requests.RequestException as ex:
-#     except requests.exceptions.RequestException as ex:
-#         print(f"[X] response.get() exception: {ex}\n    parameters: {parameters}")
-#         pass
-#     finally:
-#         response.close()
-#
-#     if response.status_code != 200:
-#         print(f"[X] Request Failed: {response.status_code}.\nText:\n{response.text}\n{parameters['function']}")
-#
-#     # If 'json' datatype, return as 'json'. Otherwise return text response for 'csv'
-#     if self.datatype == 'json':
-#         response = response.json()
-#     else:
-#         response = response.text
-
-#     if self._api_call_count < 1:
-#         self._api_call_count += 1
-
 class fxClient:
 
     def __init__(self):
@@ -62,24 +23,26 @@ class fxClient:
             'from_currency': from_currency.upper(),
             'to_currency': to_currency.upper()
         }
-        result = self.av_request(parameters)
-        return result if result is not None else None
+        return self.av_request(parameters)
 
-    def av_request(self, parameters: dict, timeout: int = 60):
+    def av_request(self, parameters: dict, timeout: int = 5):
         parameters['apikey'] = self.api_key
         try:
             response = self.session.request(
                 'GET',  
                 '/query', 
                 fields=parameters,
-                timeout=''
+                timeout=timeout
             )
         except urllib3.exceptions.NewConnectionError:
+            print('Alpha vantage api connection failed.')
             pass
         finally:
             response.close()
 
-        return None
+        if response.status_code == 200:
+            return json.loads(response.data)
+        else: return None
 
     def get_fx_batch(self, from_currencies, to_currency: str = 'USD'):
         """
@@ -88,7 +51,7 @@ class fxClient:
         :param to_currency: a string being the base currency
         :returns: list fx rates in json format
         """
-
+        
         return None
 
     def threaded_requests(self):
